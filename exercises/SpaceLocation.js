841aab518e20b28a19719f5e0bcbe6aa8afcdbbf
Jessie Grier <jessiemg2577@gmail.com>
2020-07-07 14:41:40 -0500

    Initial commit, completed exercises 1-5

---
+++ exercises/SpaceLocation.js
@@ -0,0 +1,11 @@
+// Paste in the provided code here:
+"use strict";
+exports.__esModule = true;
+var SpaceLocation = /** @class */ (function () {
+    function SpaceLocation(name, kilometersAway) {
+        this.name = name;
+        this.kilometersAway = kilometersAway;
+    }
+    return SpaceLocation;
+}());
+exports.SpaceLocation = SpaceLocation;
